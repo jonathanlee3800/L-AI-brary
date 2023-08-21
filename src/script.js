02c9316d986347dfe165fba826a65563644efdd5
jonathanlee3800 <jonathannnleee@gmail.com>
2023-08-21 14:43:29 +0800

    ui

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
-document.addEventListener("click", myFunction1);
-document.addEventListener("click", myFunction2);
+// document.addEventListener("click", myFunction1);
+// document.addEventListener("click", myFunction2);
 
-function myFunction1() {
-  document.getElementById("demo").innerHTML += "First function was executed! ";
-}
+// function myFunction1() {
+//   document.getElementById("demo").innerHTML += "First function was executed! ";
+// }
 
-function myFunction2() {
-  document.getElementById("demo").innerHTML += "Second function was executed! ";
-}
+// function myFunction2() {
+//   document.getElementById("demo").innerHTML += "Second function was executed! ";
+// }
 
 /* test push for matthias */
