707ca5bf5b86beeea88b36d447c7ef705a99954f
seanl01 <57535001+seanl01@users.noreply.github.com>
2023-08-21 15:08:05 +0800

    add functions for gpt

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("click", myFunction1);
 document.addEventListener("click", myFunction2);
 
@@ -10,3 +11,39 @@ function myFunction2() {
 }
 
 /* test push for matthias */
+async function generateInitialQuery(query){
+    // Returns promise object for json response data
+    
+    const prompt =  `generate keywords for my prompt. suggest keywords that may relate to the search query, keep important queries in the front that directly relate to the query. seperate these keywords with "OR" and keep it to 10 keywords
+    User: Poverty in Asia
+    ChatGPT: "Poverty in Asia" OR "Asian poverty rates" OR "poverty reduction initiatives Asia" OR "poverty statistics" OR "economic inequality Asia" OR "poverty alleviation programs" OR "Asia developing countries" OR "poverty challenges Asia" OR "rural poverty Asia" OR "urban poverty Asia"
+
+    User: ${query}
+    ChatGPT: `;
+
+    const OPENAI_API_KEY = "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"
+    const url = "https://api.openai.com/v1/chat/completions";
+    
+    const headers = {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${OPENAI_API_KEY}`
+    };
+      
+    const requestData = {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role:"system", content: prompt}],
+            temperature: 0.6,
+        })
+    }
+    
+    return fetch(url, requestData).then(response => response.json());
+
+}
+
+console.log(generateInitialQuery("electric cars").then(data => {console.log(data.choices[0].message.content)}))
+
+
+
