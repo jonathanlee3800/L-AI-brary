3cf594c5bca8923fd8fdbdd68d2516e5ff6a0e37
MatthiasLJK <131587203+ljkMatthias@users.noreply.github.com>
2023-08-27 06:59:59 +0800

    paragraphSearch
    run without extension popup

--- src/script.js
+++ src/script.js
@@ -21,7 +21,7 @@ const HEADERS = {
   Authorization: `Bearer ${OPENAI_API_KEY}`,
 };
 
-chrome.storage.onChanged.addListener((changes, namespace) => {
+chrome.storage.sync.onChanged.addListener((changes, namespace) => {
   for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
     console.log(
       `Storage key "${key}" in namespace "${namespace}" changed.`,
